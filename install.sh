bbb1a1092b155a3b51b19fe24909ceb6f66c853d
James Montalvo <jamesmontalvo3@gmail.com>
2015-08-02 21:32:27 -0500

    disable IR remote

--- install.sh
+++ install.sh
@@ -31,6 +31,9 @@ defaults write com.apple.print.PrintingPrefs “Quit When Finished” -bool true
 # defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
 # defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
 
+## Disable IR Remote (so Apple TV doesn't affect computer)
+defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -bool false
+
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
